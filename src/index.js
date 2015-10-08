8ccfd6994ef2f7b1f140b8a6003036fba82adbc7
Alexander Antonov <aanodov@gmail.com>
2015-10-08 23:56:35 +0300

    index.js was created

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 'use strict';
+
+var through = require('through2');
+
+/**
+ * @param {object} config
+ * @param {string} config.method - It can be one of two values: 'bgImageCutter' or
+ * 'bgImageCutterInvertor'
+ */
+module.exports = function(config) {
+    var _this = this;
+
+    this.methods = {
+        bgImageCutter: function (str) {
+            return str.replace(/background(?:-image)?[\s]*:.*url\([^\)]*\)[^;\}]*[;\}]/ig, '');
+        },
+        bgImageCutterInvertor: function (str) {
+            return str.replace(/(?:(\{)[^\}]*(background(?:-image)?[\s]*:.*url\([^\)]*\)[^;\}]*[;\}])[^\}]*(\})|(\{)[^\}]*(\}))/ig, '$1$2$3$4$5');
+        }
+    };
+
+    return through.obj(function (file, enc, callback) {
+        if (file.isBuffer()) {
+            try {
+                file.contents = _this.methods[config.method].call(this, file.contents.toString('utf-8'));
+            } catch (err) {
+                console.log('Error ' + err.name + ": " + err.message + "\n" + err.stack);
+            }
+        }
+
+        this.push(file);
+
+        callback();
+    })
+};
